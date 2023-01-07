dca360a74301f448bae88ab28f14009ed2d10269
Fredrik Welander <fredrik.welander@subsite.fi>
2023-01-07 19:31:40 +0200

    Add testCreateGraph

--- src/test/java/org/example/NodeTests.java
+++ src/test/java/org/example/NodeTests.java
@@ -7,6 +7,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class NodeTests {
     @Test
+    void testCreateGraph() {
+        var g = GraphData.createGraph();
+        assertEquals(15, g.size());
+    }
+
+    @Test
     void testCalculateH() {
         Node n1 = new Node("n1", 60.2007, 24.9662);
         Node n2 = new Node("n2", 60.1593, 24.9207);
